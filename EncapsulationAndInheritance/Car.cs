7befa2adadac8cd5698737db1c675bf99fd093f9
cancelik-dev <cancelikdev@gmail.com>
2022-07-22 11:08:28 +0300

    Encapsulation And Inheritance
    Encapsulation And Inheritance

---
+++ EncapsulationAndInheritance/Car.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncapsulationAndInheritance
+{
+    public class Car
+    {
+        public string marka;
+        public int uretim_yil;
+        private string model;
+        public string enModel
+        {
+            get
+            {
+                return model;
+            }
+            set
+            {
+                if (value.Length > 3 && value.Length < 20)
+                {
+                    model = value;
+                }
+                else
+                {
+                    model = "Error";
+                }
+            }
+        }
+
+        public void ArabaBilgiGoster()
+        {
+            Console.WriteLine("Marka: " + marka+ "\nModel: " + model + "\nÜretim yılı: " + uretim_yil);
+        }
+    }
+}
---
+++ EncapsulationAndInheritance/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace EncapsulationAndInheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Encapsulation And Inheritance");
+            Ford ford = new Ford();
+            ford.marka = "Ford";
+            ford.enModel = "Focus";
+            ford.uretim_yil = 2012;
+            ford.ArabaBilgiGoster();
+            Console.ReadKey();
+        }
+    }
+}
